1d06719be97249f0958579f0de987b4707e8d8d1
zitn <me@5a6c.me>
2023-11-27 18:18:52 +0800

    refactor(plugin): host info plugin

--- plugins/host.go
+++ plugins/host.go
@@ -1,4 +1,4 @@
-package info
+package plugins
 
 import (
 	"fmt"
@@ -6,9 +6,26 @@ import (
 
 	"github.com/gookit/color"
 	"github.com/shirou/gopsutil/v3/host"
+	"github.com/urfave/cli/v2"
+
+	"d-eyes/cmd"
 )
 
-func DisplayBaseInfo() {
+type PluginHost struct{}
+
+func init() {
+	cmd.Register(new(PluginHost).InitCommands())
+}
+
+func (p *PluginHost) InitCommands() *cli.Command {
+	return &cli.Command{
+		Name:   "host",
+		Usage:  "Command for displaying basic host information",
+		Action: p.Run,
+	}
+}
+
+func (p *PluginHost) Run(c *cli.Context) error {
 	infoStat, _ := host.Info()
 	platform := infoStat.Platform + " " + infoStat.PlatformVersion
 	OsKernel := infoStat.KernelArch + " " + infoStat.KernelVersion
@@ -23,5 +40,5 @@ func DisplayBaseInfo() {
 
 	color.Greenp("* ")
 	fmt.Println("CURRENT USER:   ", CurrentUser.Username)
-
+	return nil
 }
