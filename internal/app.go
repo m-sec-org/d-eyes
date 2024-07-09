e3b7fc855a5cf9cc1b21e54310d0d6275034e8c7
zitn <me@5a6c.me>
2024-07-09 17:04:14 +0800

    fix: add version

--- internal/app.go
+++ internal/app.go
@@ -1,7 +1,11 @@
 package internal
 
 import (
+	"fmt"
+
 	"github.com/urfave/cli/v2"
+
+	"github.com/m-sec-org/d-eyes/pkg/color"
 )
 
 var App *cli.App
@@ -10,7 +14,17 @@ func init() {
 	App = cli.NewApp()
 	App.Name = "d-eyes"
 	App.Usage = "The Eyes of Darkness from Nsfocus spy on everything."
-	App.Commands = make([]*cli.Command, 0)
+	App.Commands = []*cli.Command{
+		{
+			Name:    "version",
+			Aliases: []string{"v"},
+			Usage:   "Show the version of d-eyes",
+			Action: func(c *cli.Context) error {
+				fmt.Println(color.Green.Sprint("v1.3.0"))
+				return nil
+			},
+		},
+	}
 }
 
 // RegisterCommand 注册插件
